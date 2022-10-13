d051fa92de2a5a2b07d23027bdec0b0a29d22d89
Awesome-Gtash <115485004+Awesome-Gtash@users.noreply.github.com>
2022-10-13 20:09:06 +0530

    Update __main__.py

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -113,9 +113,9 @@ HELP_MSG = "Click The Button Below To Get Help Menu In Your Dm."
 START_MSG = "I'm Awake Already!\n<b>Haven't Slept Since:</b> <code>{}</code>"
 
 PM_START_TEXT = """
-────「 [{}](https://telegra.ph/file/4bf4b16a4e561dac5ad62.jpg) 」────
+────「 [{}](https://telegra.ph/file/0b9c23d669775450ff748.jpg) 」────
 *Hola! {},*
-*I am an Anime Themed Advance Group Management Bot With Lot Of Sexy Features.*
+*I am an Anime Themed Advance Group Management Bot With Lot Of Cool Features.*
 ➖➖➖➖➖➖➖➖➖➖➖➖➖
 ❍ *Uptime:* `{}`
 ❍ `{}` *Users, Across* `{}` *Chats.*
