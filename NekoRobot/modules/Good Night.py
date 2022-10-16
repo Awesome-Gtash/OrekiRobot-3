8cef9ab9c1f40f8c382af7413b526da24a492ea9
The Gtash <115485004+Awesome-Gtash@users.noreply.github.com>
2022-10-16 18:03:14 +0530

    Update Good Night.py

--- NekoRobot/modules/Good Night.py
+++ NekoRobot/modules/Good Night.py
@@ -26,7 +26,7 @@ from telethon import Button
 from NekoRobot import tbot as tbot
 from NekoRobot.events import register
 
-PHOTO = "https://te.legra.ph/file/4e959d8f074bef7061463.mp4"
+PHOTO = "https://te.legra.ph/file/850abda7f1142da34cd39.jpg"
 
 
 @register(pattern=("Good night"))
@@ -34,7 +34,7 @@ async def awake(event):
     NEKO = f"Good night I hope tomorrow is the best day in your life. {event.sender.first_name}"
     BUTTON = [
         [
-            Button.url(" UPDATES", "https://telegram.dog/Tiger_Updates"),
+            Button.url(" Updates 📢", "https://telegram.dog/Tiger_Updates"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
