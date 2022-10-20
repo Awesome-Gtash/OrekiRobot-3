70f9418daeb328d8cc470838129b3ff0b320a674
The Gtash <115485004+Awesome-Gtash@users.noreply.github.com>
2022-10-20 19:14:24 +0530

    Update whois.py

--- NekoRobot/modules/whois.py
+++ NekoRobot/modules/whois.py
@@ -45,13 +45,14 @@ def ReplyCheck(message: Message):
 
 
 infotext = (
-    "**{full_name}(tg://user?id={user_id})**\n"
+    " * Full Name: `(tg://user?id={user_id})´\n"
     " * User ID: `{user_id}`\n"
     " * First Name: `{first_name}`\n"
     " * Last Name: `{last_name}`\n"
     " * User Name: `{user_name}`\n"
+    " * Info Pic: True\n"
+    " * Bio: `{bio}´\n"
     " * Last Online: `{last_online}`\n"
-    " * Bio: '{bio}'\n"
 )
 
 
