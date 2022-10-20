68e4b1596368edda93811eeef8b4748a5de69f91
The Gtash <115485004+Awesome-Gtash@users.noreply.github.com>
2022-10-20 18:45:16 +0530

    Update Hi.py

--- NekoRobot/modules/Hi.py
+++ NekoRobot/modules/Hi.py
@@ -4,19 +4,9 @@ from NekoRobot import tbot as bot
 from NekoRobot import tbot as tgbot
 from NekoRobot.events import register
 
-edit_time = 5
-""" =======================CONSTANTS====================== """
-file1 = "https://te.legra.ph/file/84696d6e1bebe28976e5a.jpg"
-file2 = "https://te.legra.ph/file/f30b3e091288e185414a3.jpg"
-file3 = "https://te.legra.ph/file/4624003c955098d4a4613.jpg"
-file4 = "https://te.legra.ph/file/f5856e243e9c03d1ed501.jpg"
-file5 = "https://te.legra.ph/file/4406fa4db464bf8316c7f.jpg"
-""" =======================CONSTANTS====================== """
+PHOTO = "https://te.legra.ph/file/4406fa4db464bf8316c7f.jpg"
 
-
-@register(pattern="/hi")
-
-@register(pattern=("Hello"))
+@register(pattern=("Hi"))
 async def awake(event):
     NEKO = f"{event.sender.first_name} Hello Friend How are you?!"
     BUTTON = [
@@ -25,33 +15,6 @@ async def awake(event):
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
-    
-    await asyncio.sleep(edit_time)
-    ok = await bot.edit_message(event.chat_id, on, file=file3, buttons=button)
-
-    await asyncio.sleep(edit_time)
-    ok2 = await bot.edit_message(event.chat_id, ok, file=file5, buttons=button)
-
-    await asyncio.sleep(edit_time)
-    ok3 = await bot.edit_message(event.chat_id, ok2, file=file1, buttons=button)
-
-    await asyncio.sleep(edit_time)
-    ok7 = await bot.edit_message(event.chat_id, ok6, file=file4, buttons=button)
-
-    await asyncio.sleep(edit_time)
-    ok4 = await bot.edit_message(event.chat_id, ok3, file=file2, buttons=button)
-
-    await asyncio.sleep(edit_time)
-    ok5 = await bot.edit_message(event.chat_id, ok4, file=file1, buttons=button)
-
-    await asyncio.sleep(edit_time)
-    ok6 = await bot.edit_message(event.chat_id, ok5, file=file3, buttons=button)
-
-    await asyncio.sleep(edit_time)
-    ok7 = await bot.edit_message(event.chat_id, ok6, file=file5, buttons=button)
-
-    await asyncio.sleep(edit_time)
-    ok7 = await bot.edit_message(event.chat_id, ok6, file=file4, buttons=button)
 
 __help__ = """
 /hi: It will Reply as hi.
