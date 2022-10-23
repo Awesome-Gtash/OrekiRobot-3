51f178b24c5f037a0531cbdedf5f9dc87f2733ad
The Gtash <115485004+Awesome-Gtash@users.noreply.github.com>
2022-10-23 17:35:30 +0530

    Create Thanks.py

---
+++ NekoRobot/modules/Thanks.py
@@ -0,0 +1,40 @@
+"""
+MIT License
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2022 Awesome-Prince
+Copyright (c) 2022, Koy ki   Network, <https://github.com/Awesome-Prince/NekoRobot-3>
+This file is part of @NekoXRobot (Telegram Bot)
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the Software), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from telethon import Button
+
+from NekoRobot import tbot as tbot
+from NekoRobot.events import register
+
+PHOTO = "https://te.legra.ph/file/dd128ece3d79f8321aa11.jpg"
+
+
+@register(pattern=("Thanks"))
+async def awake(event):
+    NEKO = f" {event.sender.first_name} Thank You So Much!"
+    BUTTON = [
+        [
+            Button.url("  Updates 📢", "https://telegram.dog/Tiger_Updates"),
+        ]
+    ]
+    await tbot.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
