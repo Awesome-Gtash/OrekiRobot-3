bd22c059107c04ad7f7a9bbf7168012d2e7c7e91
The Gtash <115485004+Awesome-Gtash@users.noreply.github.com>
2022-10-25 01:29:54 +0530

    Update Good Night.py

--- NekoRobot/modules/Good Night.py
+++ NekoRobot/modules/Good Night.py
@@ -34,7 +34,7 @@ async def awake(event):
     NEKO = f"Good night I hope tomorrow is the best day in your life. {event.sender.first_name}"
     BUTTON = [
         [
-            Button.url(" Updates 📢", "https://telegram.dog/Tiger_Updates"),
+            Button.url(" Support 💌", "https://telegram.dog/Tiger_SupportChat"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
