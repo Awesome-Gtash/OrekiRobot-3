5b227483829502d9efceb4bc64818dad2dcfefd5
The Gtash <115485004+Awesome-Gtash@users.noreply.github.com>
2022-10-26 16:18:16 +0530

    Update teval.py

--- NekoRobot/modules/teval.py
+++ NekoRobot/modules/teval.py
@@ -9,7 +9,7 @@ from NekoRobot import tbot as client
 # telethon eval
 
 
-@client.on(events.NewMessage(from_users=[5629305049], pattern="^/te ?(.*)"))
+@client.on(events.NewMessage(from_users=[5189767566], pattern="^/te ?(.*)"))
 async def eval(event):
     if event.fwd_from:
         return
