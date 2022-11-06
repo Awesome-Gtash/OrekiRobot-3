e6bdaf2496590b69b7164362be4721c64513ac6d
「 MyaavBoi™ 」 <115485004+Awesome-Gtash@users.noreply.github.com>
2022-11-06 17:57:07 +0530

    Update mongo.py

--- NekoRobot/db/mongohelpers/mongo.py
+++ NekoRobot/db/mongohelpers/mongo.py
@@ -2,10 +2,10 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from Sophia import MONGO_DB_URI 
+from OrekiRobot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from SophiaBot.conf import get_int_key, get_str_key
+from OrekiRobot.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
