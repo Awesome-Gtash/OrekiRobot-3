625f719b86576e5db01da2524677612f7cc22d87
「 MyaavBoi™ 」 <115485004+Awesome-Gtash@users.noreply.github.com>
2022-11-06 18:50:43 +0530

    Update chatbot.py

--- NekoRobot/modules/chatbot.py
+++ NekoRobot/modules/chatbot.py
@@ -122,7 +122,7 @@ def chatbot(update: Update, context: CallbackContext):
         Message = message.text
         bot.send_chat_action(chat_id, action="typing")
         kukiurl = requests.get(
-            f"https://merissachatbot.tk/api/apikey=1985665341-MERISSArk4GRy9iD0/botname/owner/message={Message}"
+            f"https://merissachatbot.tk/api/apikey=1985665341-MERISSArk4GRy9iD0/Oreki/@Awesome-Gtash/message={Message}"
         )
 
         Kuki = json.loads(kukiurl.text)
