dd6ce1a7b87f7bbd08a93fe1f93f26629e7049c4
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-11-12 14:29:34 +0530

    Update OrekiStartMessage.py

--- NekoRobot/modules/OrekiStartMessage.py
+++ NekoRobot/modules/OrekiStartMessage.py
@@ -3,7 +3,7 @@ from telegram import __version__ as ptb
 from telethon import __version__ as tlhver
 
 OREKI = f"""
-Prince Oreki 왕자 Is Started!
+Prince Oreki 왕자 Is Started [!](https://te.legra.ph/file/5aadfffa390146c1fb9a2.jpg)
 
 ➪ Uptime:</b> <code>{}</code>
 ➪ Python Vesion:</b> <code>{}</code>
