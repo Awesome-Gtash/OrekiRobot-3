52fc311f70a0486824660af20dbcea5ae3a529b7
「 MyaavBoi™ 」 <115485004+Awesome-Gtash@users.noreply.github.com>
2022-11-12 20:39:23 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE NekoRobot
-:: Enables virtual env mode and then starts Neko
-env\scripts\activate.bat && py -m NekoRobot
+TITLE OrekiRobot
+:: Enables virtual env mode and then starts Oreki
+env\scripts\activate.bat && py -m OrekiRobot
