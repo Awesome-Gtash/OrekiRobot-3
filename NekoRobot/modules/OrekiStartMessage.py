60dc623e9edb3b6149c7f674de157f0ee77d839b
「 MyaavBoi™ 」 <115485004+Awesome-Gtash@users.noreply.github.com>
2022-11-12 13:42:22 +0530

    Update OrekiStartMessage.py

--- NekoRobot/modules/OrekiStartMessage.py
+++ NekoRobot/modules/OrekiStartMessage.py
@@ -1,5 +1,12 @@
+from pyrogram import __version__ as pyrover
+from telethon import __version__ as tlher
+from telegram import __version__ as ptb
+from telethon import Button
+
+START_PIC = "https://te.legra.ph/file/af9b448b3f64853a806d7.jpg"
+
 OREKI = """
-[Prince Oreki 왕자 Is Started!](https://te.legra.ph/file/af9b448b3f64853a806d7.jpg)
+Prince Oreki 왕자 Is Started!
 
 ➪ Uptime:</b> <code>{}</code>
 ➪ Python Vesion:</b> <code>{}</code>
@@ -7,3 +14,10 @@ OREKI = """
 ➪ Telethon Version: {tlhver}
 ➪ Python-Telegram-Bot Version: {ptb}
 """
+
+BUTTON = [
+        [
+            Button.url("  Help 🎗️", "https://t.me/OrekiXProRobot?start=help"),
+        ]
+    ]
+    tbot.send_file(-1001878260997, START_PIC, caption=OREKI, buttons=BUTTON)
