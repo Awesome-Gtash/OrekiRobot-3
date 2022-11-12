1692e4f0fe71a5e32447b8620053575831ca7011
「 LovelyPrince™ 」 <DarlingPrince@protonmail.com>
2022-11-12 14:32:49 +0530

    Update OrekiStartMessage.py

--- NekoRobot/modules/OrekiStartMessage.py
+++ NekoRobot/modules/OrekiStartMessage.py
@@ -3,7 +3,7 @@ Prince Oreki 왕자 Is Started [!](https://te.legra.ph/file/5aadfffa390146c1fb9a
 
 ➪ Uptime: I'm Always Awaken✨👀
 ➪ Python Vesion: 3.0.6
-➪ Pyrogram Version: None
-➪ Telethon Version: None
+➪ Pyrogram Version: 2.0.40
+➪ Telethon Version: 1.24.0
 ➪ Python-Telegram-Bot Version: 13.14
 """
