709289a4b380915551eff7cd3d88065f0f71a1b8
「 MyaavBoi™ 」 <115485004+Awesome-Gtash@users.noreply.github.com>
2022-11-12 13:30:40 +0530

    Update OrekiStartMessage.py

--- NekoRobot/modules/OrekiStartMessage.py
+++ NekoRobot/modules/OrekiStartMessage.py
@@ -3,7 +3,7 @@ from telegram import __version__ as ptb
 from telethon import __version__ as tlhver
 from telethon import button
 
-OREKI = f"""
+OREKI = """
 [Prince Oreki 왕자 Is Started!](https://te.legra.ph/file/af9b448b3f64853a806d7.jpg)
 
 ➪ Uptime:</b> <code>{}</code>
