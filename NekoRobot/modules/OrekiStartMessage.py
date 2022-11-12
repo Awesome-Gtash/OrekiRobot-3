b03dcca5222b97afa413368269c3d1a13ce55000
「 MyaavBoi™ 」 <115485004+Awesome-Gtash@users.noreply.github.com>
2022-11-12 12:30:41 +0530

    Update OrekiStartMessage.py

--- NekoRobot/modules/OrekiStartMessage.py
+++ NekoRobot/modules/OrekiStartMessage.py
@@ -21,4 +21,4 @@ BUTTON = [
         Button.url("Help 🎗️", "https//t.me/OrekiXProRobot?start=help"),
     ]
 ]
-await tbot.send file(-1001878260997, START_PIC, caption=OREKI, buttons=BUTTON)
+await tbot.send_file(-1001878260997, START_PIC, caption=OREKI, buttons=BUTTON)
