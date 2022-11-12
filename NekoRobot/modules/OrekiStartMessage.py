59a8f2658a5b3f30b19f2eee97044a6f37be7eac
Prince <90893568+Awesome-Prince@users.noreply.github.com>
2022-11-12 08:01:05 +0000

    Auto Code Formatting

--- NekoRobot/modules/OrekiStartMessage.py
+++ NekoRobot/modules/OrekiStartMessage.py
@@ -1,8 +1,3 @@
-from pyrogram import __version__ as pyrover
-from telegram import __version__ as ptb
-from telethon import __version__ as tlhver
-from telethon import button
-
 OREKI = """
 [Prince Oreki 왕자 Is Started!](https://te.legra.ph/file/af9b448b3f64853a806d7.jpg)
 
