7f4f34ac6659ea091175e2fe95b661d60097af86
「 MyaavBoi™ 」 <115485004+Awesome-Gtash@users.noreply.github.com>
2022-11-13 11:05:29 +0530

    Delete NekoRobot/db/mongohelpers directory

--- NekoRobot/db/mongohelpers/mongo2.py
+++
@@ -1,9 +0,0 @@
-# Mongo db for text filters
-
-from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-from OrekiRobot import MONGO_DB_URI
-
-MONGO = MONGO_DB_URI
-
-mongo_client = MongoClient(MONGO)
-db = mongo_client.OrekiRobot
--- NekoRobot/db/mongohelpers/mongo3.py
+++
@@ -1,6 +0,0 @@
-from OrekiRobot import MONGO_DB_URI
-from pymongo import MongoClient
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI)
-db = client["OrekiRobot"]
