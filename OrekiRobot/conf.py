d9046ce894fa580405f66b006f7e429d016bc170
「 MyaavBoi™ 」 <115485004+Awesome-Gtash@users.noreply.github.com>
2022-11-14 22:55:56 +0530

    Auto Code Formatting

---
+++ OrekiRobot/conf.py
@@ -0,0 +1,31 @@
+from envparse import env
+
+from OrekiRobot import LOGGER
+
+DEFAULTS = {
+    "LOAD_MODULES": True,
+}
+
+
+def get_str_key(name, required=False):
+    default = DEFAULTS[name] if name in DEFAULTS else None
+    if not (data := env.str(name, default=default)) and not required:
+        LOGGER.warn(f"No str key: {name}")
+        return None
+    elif not data:
+        LOGGER.critical(f"No str key: {name}")
+        sys.exit(2)
+    else:
+        return data
+
+
+def get_int_key(name, required=False):
+    default = DEFAULTS[name] if name in DEFAULTS else None
+    if not (data := env.int(name, default=default)) and not required:
+        LOGGER.warn(f"No int key: {name}")
+        return None
+    elif not data:
+        LOGGER.critical(f"No int key: {name}")
+        sys.exit(2)
+    else:
+        return data
