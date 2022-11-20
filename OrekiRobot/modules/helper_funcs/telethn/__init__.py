0ecbfdb1efffa1c880ae9e143ccb482daa52c9b4
「 MyaavBoi™ 」 <115485004+Awesome-Gtash@users.noreply.github.com>
2022-11-20 09:57:45 +0530

    Initial Commit v3

---
+++ OrekiRobot/modules/helper_funcs/telethn/__init__.py
@@ -0,0 +1,7 @@
+from OrekiRobot import DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES, tbot
+
+IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS + DEV_USERS
+
+IMMUNE_USERS = (
+    list(DRAGONS) + list(WOLVES) + list(DEMONS) + list(TIGERS) + list(DEV_USERS)
+)
