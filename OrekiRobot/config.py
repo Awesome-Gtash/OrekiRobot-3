c7309331c7f22ee0e56606f509f465aebc0aef32
「 MyaavBoi™ 」 <115485004+Awesome-Gtash@users.noreply.github.com>
2022-12-11 20:59:05 +0530

    `Initial commit v3`

--- OrekiRobot/config.py
+++ OrekiRobot/config.py
@@ -17,7 +17,7 @@ class Config(object):
     API_ID = 1234567  # integer value, dont use ""
     API_HASH = "123458494"
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 5189767566  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 5534661034  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Awesome_Gtashxd"
     SUPPORT_CHAT = "Dev_SupportChat"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
@@ -100,9 +100,9 @@ class Config(object):
     API_ID = 1234567  # integer value, dont use ""
     API_HASH = "12345abcdefg23459"
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 5189767566  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Awesome_MB"
-    SUPPORT_CHAT = "Tiger_SupportChat"  # Your own group for support, do not add the @
+    OWNER_ID = 5534661034  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Awesome_Gtashxd"
+    SUPPORT_CHAT = "Dev_SupportChat"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001852609544
     )  # Prints any new group the bot is added to, prints just the name and ID.
