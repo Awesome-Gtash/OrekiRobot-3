5c8a42593999f5e30e69a5b28aad3805a3b832d4
「 MyaavBoi™ 」 <115485004+Awesome-Gtash@users.noreply.github.com>
2023-01-14 16:22:13 +0530

    Create waifu.py

---
+++ OrekiRobot/modules/waifu.py
@@ -0,0 +1,26 @@
+import requests
+from telegram.constants import ParseMode
+
+from OrekiRobot.events import register as bot
+
+
+@bot(pattern="[/!]waifu")
+async def ok(event):
+    url = "https://api.waifu.pics/sfw/waifu"
+    r = requests.get(url)
+    e = r.json()
+    await event.reply(
+        "**A waifu appeared!** \nAdd them to your harem by sending /protecc character name",
+        parse_mode=ParseMode.MARKDOWN,
+        file=e["url"])
+
+
+@bot(pattern="[/!]protecc")
+async def ok(event):
+    await event.reply(
+        "OwO you protecc'd A Waifu This waifu has been added to your harem.")
+
+
+@bot(pattern="[/!]harem")
+async def ok(event):
+    await event.reply("You haven't protecc'd any waifu yet...")
