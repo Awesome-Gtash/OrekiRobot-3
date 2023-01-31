00fc8773b4bfb9008c4d7b968d7099ef6b60ac82
「 MyaavBoi™ 」 <115485004+Awesome-Gtash@users.noreply.github.com>
2023-01-31 23:13:28 +0530

    Create Varisu.py

---
+++ OrekiRobot/modules/Varisu.py
@@ -0,0 +1,22 @@
+from OrekiRobot import tbot as tbot
+from OrekiRobot.events import register
+
+@register(pattern=("Varisu"))
+async def awake(event):
+ await event.reply("Uploading...💫")
+
+IMAGE = "https://te.legra.ph/file/191a2b533e49ddd8f63cc.jpg"
+
+
+@register(pattern=("Varisu"))
+async def awake(event):
+    OREKI = """
+*Name : Varisu*
+*Year : 2023*
+*Lang : Tamil*
+*Quality : HQ PreDVD*
+*Size : 450MB - 700MB*
+"""
+
+file1 = "
+              
