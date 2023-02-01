247ab212dd861b4afd1b784477150039f6f8e035
「 MyaavBoi™ 」 <115485004+Awesome-Gtash@users.noreply.github.com>
2023-02-01 13:59:05 +0530

    Update Varisu.py

--- OrekiRobot/modules/Varisu.py
+++ OrekiRobot/modules/Varisu.py
@@ -7,16 +7,77 @@ async def awake(event):
 
 IMAGE = "https://te.legra.ph/file/191a2b533e49ddd8f63cc.jpg"
 
+@register(pattern=("Varisu"))
+async def awake(event):
+    IMG_CAP_OREKI = """
+**Name : Varisu**
+**Year : 2023**
+**Lang : Tamil**
+**Quality : HQ PreDVD**
+**Size : 450MB - 700MB**
+"""
+
+
+
+file1 = "https://t.me/OrekiMovies/6"
+
+FILE1_CAP_OREKI = """
+**Varisu (2023) HQ PreDVD - x264 - Tamil**
+
+**➠ Uploaded by :** @OrekiProXBot
+"""
+@register(pattern=("Varisu"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, file1, FILE1_CAP_OREKI)
+
+
+file2 = ""
+
+FILE2_CAP_OREKI = """
+**Varisu (2023) HQ PreDVD - x264 - Tamil**
+
+**➠ Uploaded by :** @OrekiProXBot
+"""
+@register(pattern=("Varisu"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, file2, FILE2_CAP_OREKI)
+
+
+file3 = ""
+
+FILE3_CAP_OREKI = """
+**Varisu (2023) HQ PreDVD - x264 - Tamil**
+
+**➠ Uploaded by :** @OrekiProXBot
+"""
+@register(pattern=("Varisu"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, file3, FILE3_CAP_OREKI)
+
+
+file4 = ""
+
+FILE4_CAP_OREKI = """
+**Varisu (2023) HQ PreDVD - x264 - Tamil**
+
+**➠ Uploaded by :** @OrekiProXBot
+"""
+@register(pattern=("Varisu"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, file4, FILE4_CAP_OREKI)
+
+
+file5 = ""
+
+FILE5_CAP_OREKI = """
+**Varisu (2023) HQ PreDVD - x264 - Tamil**
+
+**➠ Uploaded by :** @OrekiProXBot
+"""
+@register(pattern=("Varisu"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, file5, FILE5_CAP_OREKI)
 
 @register(pattern=("Varisu"))
 async def awake(event):
-    OREKI = """
-*Name : Varisu*
-*Year : 2023*
-*Lang : Tamil*
-*Quality : HQ PreDVD*
-*Size : 450MB - 700MB*
-"""
-
-file1 = "
-              
+  await event.reply("➠ Uploaded by : @OrekiProXBot")
