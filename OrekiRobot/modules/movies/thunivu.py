a5dca5e04031097acc019102e4143cc305e73a7b
《•The Gtash•》 <115485004+Awesome-Gtash@users.noreply.github.com>
2023-02-05 13:47:25 +0530

    Update thunivu.py

--- OrekiRobot/modules/movies/thunivu.py
+++ OrekiRobot/modules/movies/thunivu.py
@@ -25,7 +25,8 @@ FIRST_BUTTON = [
 
 def convert(seconds): 
     seconds = seconds % (24 * 3600) 
-    hour = seconds // 3600 seconds %= 3600 
+    hour = seconds // 3600 
+    seconds %= 3600 
     minutes = seconds // 60 
     seconds %= 60 
     return "%d:%02d:%02d" % (hour, minutes, seconds)
