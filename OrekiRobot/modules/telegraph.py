879df9fe9d24c4f224a60b047ffa4c23533d1b1f
《•The Gtash•》 <115485004+Awesome-Gtash@users.noreply.github.com>
2023-02-08 10:13:54 +0530

    By Gtash Network
    Signed-off-by: 《•The Gtash•》 <115485004+Awesome-Gtash@users.noreply.github.com>

--- OrekiRobot/modules/telegraph.py
+++ OrekiRobot/modules/telegraph.py
@@ -2,33 +2,33 @@ import os
 from datetime import datetime
 
 from PIL import Image
-from Telegraph import graph, exceptions, upload_file
+from telegraph import Telegraph, exceptions, upload_file
 from telethon import types
 
-from OrekiRobot import tbot
+from OrekiRobot import tbot as oreki
 from OrekiRobot.events import register
 
 TMP_DOWNLOAD_DIRECTORY = "tg-File/"
 babe = "OrekiRobot"
-Telegraph = Telegraph()
-data = graph.create_account(short_name=babe)
+telegraph = Telegraph()
+data = telegraph.create_account(short_name=babe)
 auth_url = data["auth_url"]
 
 
-@register(pattern="^/tm ?(.*)")
+@register(pattern="^/t(m|gt) ?(.*)")
 async def telegrap(event):
     optional_title = event.pattern_match.group(2)
     if event.reply_to_msg_id:
         datetime.now()
         reply_msg = await event.get_reply_message()
         input_str = event.pattern_match.group(1)
-        if input_str == "gm":
-            downloaded_file_name = await tbot.download_media(
+        if input_str == "m":
+            downloaded_file_name = await oreki.download_media(
                 reply_msg, TMP_DOWNLOAD_DIRECTORY
             )
             datetime.now()
             if not downloaded_file_name:
-                await tbot.send_message(event.chat_id, "Not Supported Format Media!")
+                await oreki.send_message(event.chat_id, "Not Supported Format Media!")
                 return
             if downloaded_file_name.endswith((".webp")):
                 resize_image(downloaded_file_name)
@@ -41,14 +41,14 @@ async def telegrap(event):
             else:
                 datetime.now()
                 os.remove(downloaded_file_name)
-                await tbot.send_message(
+                await oreki.send_message(
                     event.chat_id,
-                    "Your graph.link is now uploaded!",
+                    "Your link is completely uploaded!",
                     buttons=[
                         [
                             types.KeyboardButtonUrl(
-                                "➡ Click here to copy",
-                                f"https://graph.org{media_urls[0]}",
+                                "Click Here✅",
+                                f"https://te.legra.ph{media_urls[0]}",
                             )
                         ]
                     ],
@@ -65,8 +65,8 @@ async def telegrap(event):
                 if page_content != "":
                     pass
                 else:
-                    await tbot.send_message(event.chat_id, "Not Supported Format Text!")
-                downloaded_file_name = await tbot.download_media(
+                    await oreki.send_message(event.chat_id, "Not Supported Format Text!")
+                downloaded_file_name = await oreki.download_media(
                     reply_msg, TMP_DOWNLOAD_DIRECTORY
                 )
                 m_list = None
@@ -77,21 +77,21 @@ async def telegrap(event):
                 os.remove(downloaded_file_name)
             page_content = page_content.replace("\n", "<br>")
             datetime.now()
-            await tbot.send_message(
+            await oreki.send_message(
                 event.chat_id,
-                "Your graph.link is now uploaded!",
+                "Your link is completely uploaded!",
                 buttons=[
                     [
                         types.KeyboardButtonUrl(
-                            "➡ Click here to copy",
-                            f"https://graph.org{media_urls[0]}",
+                            "Click Here✅",
+                            f"https://telegra.ph{media_urls[0]}",
                         )
                     ]
                 ],
             )
 
     else:
-        await event.reply("Reply to a Media to get a Permanent graph.link")
+        await event.reply("Reply to a message to get a permanent link.")
 
 
 def resize_image(image):
@@ -99,4 +99,4 @@ def resize_image(image):
     im.save(image, "PNG")
 
 
-__mod_name__ = "TM"
+__mod_name__ = "Telegraph"
