3ba0b9ff426bcf42f75994eac7a202fe008ee855
《•The Gtash•》 <115485004+Awesome-Gtash@users.noreply.github.com>
2023-02-14 20:26:17 +0530

    Update config.py
    Signed-off-by: 《•The Gtash•》 <115485004+Awesome-Gtash@users.noreply.github.com>

--- OrekiRobot/config.py
+++ OrekiRobot/config.py
@@ -19,7 +19,7 @@ class Config(object):
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 5534661034  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Awesome_Gtashxd"
-    SUPPORT_CHAT = "Dev_SupportChat"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "Gtash_Associasion"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001852609544
     )  # Prints any new group the bot is added to, prints just the name and ID.
@@ -102,7 +102,7 @@ class Config(object):
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 5534661034  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Awesome_Gtashxd"
-    SUPPORT_CHAT = "Dev_SupportChat"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "Gtash_Association"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001852609544
     )  # Prints any new group the bot is added to, prints just the name and ID.
