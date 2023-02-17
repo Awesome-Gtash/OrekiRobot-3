002b74eae5510ae8fadca2cc933f19b19a18cd44
《•The Gtash•》 <115485004+Awesome-Gtash@users.noreply.github.com>
2023-02-17 22:09:47 +0530

    Update owner.py
    Signed-off-by: 《•The Gtash•》 <115485004+Awesome-Gtash@users.noreply.github.com>

--- OrekiRobot/modules/owner.py
+++ OrekiRobot/modules/owner.py
@@ -1,22 +1,25 @@
-from telethon import Button
-
-
+from OrekiRobot import(
+BOT_NAME
+BOT_USERNAME
+OWNER_ID
+OWNER_USERNAME
+)
 
 from OrekiRobot import tbot as oreki
 from OrekiRobot.events import register as oreki
 
-PHOTO = "https://te.legra.ph/file/a3c11493fda2ae1e7b63a.jpg"
+OWNER_PIC = "https://te.legra.ph/file/a3c11493fda2ae1e7b63a.jpg"
 
 
 @oreki.on.message(pattern=("/owner"))
 async def awake(event):
-    OREKI = """
+    OWNER = """
     **╒═══「• OWNER INFO • 」**
 
-🖤 **ID:** 5534661034
+🖤 **ID:** {OWNER_ID}
 🖤 **First Name:** 【🇮🇳】𝙏𝙝𝙚 𝙂𝙩𝙖𝙨𝙝
 🖤 **Second Name:** None
-🖤 **Username:** @Awesome_Gtashxd
+🖤 **Username:** {OWNER_USERNAME}
 🖤 **Github:** [Awesome-Gtash](https://Github.com/Awesome-Gtash)
 🖤 **Instagram:** [Awesome_Gtash](https://Instagram.com/Awesome_Gtash)
 🖤 **YouTube:** [Gtash Universe](https://YouTube.com/@Gtash_Universe)
@@ -24,3 +27,27 @@ async def awake(event):
 🎗 **SUPPORT:** [Gtash Association](https://telegram.dog/Gtash_Association)
 🎗 **UPDATES:** [Gtash Updates](https://telegram.dog/Gtash_Updates)
 """
+
+await oreki.send_file(event.chat_id, OWNER_PIC, caption=OWNER)
+
+__mod_name__: Owner
+
+BOT_PIC = "https://te.legra.ph/file/cf81f114b4e7f6bbf269e.jpg"
+
+@oreki.on.message(pattern=("/bot"))
+async def awake(event):
+    BOT = """
+    **╒═══「• BOT INFO • 」**
+
+🖤 **First Name:** {BOT_NAME}
+🖤 **Second Name:** None
+🖤 **Username:** {BOT_USERNAME}
+🖤 **Github:** [OrekiRobot-3](https://Github.com/Awesome-Gtash/OrekiRobot-3.git)
+
+🎗 **SUPPORT:** [Gtash Association](https://telegram.dog/Gtash_Association)
+🎗 **UPDATES:** [Gtash Updates](https://telegram.dog/Gtash_Updates)
+"""
+
+await oreki.send_file(event.chat_id, BOT_PIC, caption=BOT)
+
+__mod_name__: Bot
