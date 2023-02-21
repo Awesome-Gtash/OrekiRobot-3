e7ff9a36747bb965de5c12c8e985c3bcb476f943
《•The Gtash•》 <115485004+Awesome-Gtash@users.noreply.github.com>
2023-02-21 13:56:12 +0530

    Update spaminfo.py
    Signed-off-by: 《•The Gtash•》 <115485004+Awesome-Gtash@users.noreply.github.com>

--- OrekiRobot/modules/spaminfo.py
+++ OrekiRobot/modules/spaminfo.py
@@ -4,7 +4,7 @@ import time
 import requests
 import telegram
 
-from OrekiRobot.modules.helper_funcs.decorators import nekocmd
+from OrekiRobot.modules.helper_funcs.decorators import orekicmd
 from OrekiRobot.modules.helper_funcs.extraction import extract_user
 
 
