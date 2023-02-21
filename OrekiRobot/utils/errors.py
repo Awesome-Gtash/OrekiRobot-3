18917f514ffa2eb46a678ef606abb3488298dc7d
《•The Gtash•》 <115485004+Awesome-Gtash@users.noreply.github.com>
2023-02-21 11:54:49 +0530

    Update errors.py
    Signed-off-by: 《•The Gtash•》 <115485004+Awesome-Gtash@users.noreply.github.com>

--- OrekiRobot/utils/errors.py
+++ OrekiRobot/utils/errors.py
@@ -4,7 +4,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from Hydra import pgram as app
+from OrekiRobot import pgram as app
 
 LOG_GROUP_ID = int(-1001681859325)
 
